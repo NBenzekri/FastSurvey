1771e3f6c409a69d385b4e305c59d1299cb67841
NBenzekri <nouriddin.benzekri@gmail.com>
2022-01-24 05:05:25 +0100

    handle BadRequestException and clean properties

---
+++ src/main/java/com/nbenzekri/fastsurvey/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.nbenzekri.fastsurvey.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
